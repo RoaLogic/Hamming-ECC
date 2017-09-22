6fdca7d2b243240c1bab2aa54b3a1c2a4398faba
Richard Herveille <richard.herveille@roalogic.com>
2017-09-22 17:29:06 +0200

    Updated Header

--- rtl/verilog/ecc_enc.sv
+++ rtl/verilog/ecc_enc.sv
@@ -1,47 +1,69 @@
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//    ██████╗  ██████╗  █████╗                                 //
-//    ██╔══██╗██╔═══██╗██╔══██╗                                //
-//    ██████╔╝██║   ██║███████║                                //
-//    ██╔══██╗██║   ██║██╔══██║                                //
-//    ██║  ██║╚██████╔╝██║  ██║                                //
-//    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝                                //
-//          ██╗      ██████╗  ██████╗ ██╗ ██████╗              //
-//          ██║     ██╔═══██╗██╔════╝ ██║██╔════╝              //
-//          ██║     ██║   ██║██║  ███╗██║██║                   //
-//          ██║     ██║   ██║██║   ██║██║██║                   //
-//          ███████╗╚██████╔╝╚██████╔╝██║╚██████╗              //
-//          ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝              //
-//                                                             //
-//    Memory ECC encoder                                       //
-//    Parameterized Extended Hamming Code Encoder              //
-//                                                             //
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//    Copyright (C) 2014-2017 ROA Logic BV                     //
-//    www.roalogic.com                                         //
-//                                                             //
-//   This source file may be used and distributed without      //
-// restriction provided that this copyright statement is not   //
-// removed from the file and that any derivative work contains //
-// the original copyright notice and the associated disclaimer.//
-//                                                             //
-//     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     //
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   //
-// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   //
-// FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      //
-// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         //
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    //
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   //
-// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        //
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  //
-// LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  //
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  //
-// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         //
-// POSSIBILITY OF SUCH DAMAGE.                                 //
-//                                                             //
-/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//                                             `---'               //
+//   Error Correction and Detection Encoder                        //
+//   Parameterized Extended Hamming Code Encoder                   //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//             Copyright (C) 2017 ROA Logic BV                     //
+//             www.roalogic.com                                    //
+//                                                                 //
+//   This source file may be used and distributed without          //
+//   restriction provided that this copyright statement is not     //
+//   removed from the file and that any derivative work contains   //
+//   the original copyright notice and the associated disclaimer.  //
+//                                                                 //
+//      THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY        //
+//   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED     //
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     //
+//   FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR OR     //
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  //
+//   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  //
+//   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  //
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)      //
+//   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     //
+//   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR  //
+//   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS          //
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
 
+// +FHDR -  Semiconductor Reuse Standard File Header Section  -------
+// FILE NAME      : ecc_enc.sv
+// DEPARTMENT     :
+// AUTHOR         : rherveille
+// AUTHOR'S EMAIL :
+// ------------------------------------------------------------------
+// RELEASE HISTORY
+// VERSION DATE        AUTHOR      DESCRIPTION
+// 1.0     2017-04-07  rherveille  initial release
+// ------------------------------------------------------------------
+// KEYWORDS : HAMMING ERROR CORRECTION ENCODER                 
+// ------------------------------------------------------------------
+// PURPOSE  : Adds ECC bits to incoming data
+// ------------------------------------------------------------------
+// PARAMETERS
+//  PARAM NAME        RANGE  DESCRIPTION              DEFAULT UNITS
+//  K                 1+     Information vector size  8
+//  P0_LSB            0,1    0: p0 located at MSB     1
+//                           1: p0 located at LSB
+// ------------------------------------------------------------------
+// REUSE ISSUES 
+//   Reset Strategy      : none
+//   Clock Domains       : none
+//   Critical Timing     :
+//   Test Features       : na
+//   Asynchronous I/F    : yes
+//   Scan Methodology    : na
+//   Instantiations      : none
+//   Synthesizable (y/n) : Yes
+//   Other               :                                         
+// -FHDR-------------------------------------------------------------
 
 
 //-----------------------------------------------------------------------------
